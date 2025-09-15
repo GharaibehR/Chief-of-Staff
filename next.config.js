14d25afaad4e0a4c4efaff86f3f1c58dc97c8174
GharaibehR <70290707+GharaibehR@users.noreply.github.com>
2025-09-15 09:18:03 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'graph.microsoft.com',
+      'media.licdn.com',
+    ],
+    formats: ['image/webp', 'image/avif'],
+  },
+  
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
